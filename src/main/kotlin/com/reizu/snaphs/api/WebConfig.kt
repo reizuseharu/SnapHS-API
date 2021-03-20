6c65216317c44530e2781570a0204b8247459449
Reizu Seharu <reizu.seharu@gmail.com>
2021-03-19 22:12:45 -0400

    {feature} CORS

---
+++ src/main/kotlin/com/reizu/snaphs/api/WebConfig.kt
@@ -0,0 +1,15 @@
+package com.reizu.snaphs.api
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
